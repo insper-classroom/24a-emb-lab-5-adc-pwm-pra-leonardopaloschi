38e2a524cfa1e832d08ec8246f05a1a6d64b38f3
leonardopaloschi <leonardopaloschi@outlook.com>
2024-04-08 19:20:39 +0000

    teste

--- main/main.c
+++ main/main.c
@@ -15,6 +15,14 @@
 
 QueueHandle_t xQueueAdc;
 
+#define ADC_CHANNELY 0
+#define ADC_CHANNELX 1
+#define deadzone 150
+
+const int ADC_PINY = 26;
+const int ADC_PINX = 27;
+
+
 typedef struct adc {
     int axis;
     int val;
@@ -25,16 +33,69 @@ void uart_task(void *p) {
     adc_t data;
 
     while (1) {
-        xQueueReceive(xQueueAdc, &data, portMAX_DELAY);
+        if(xQueueReceive(xQueueAdc, &data, portMAX_DELAY)){
+            int value = data.val/100;
+            int MSB = value >> 8;
+            int LSB = value & 0xFF;
+
+            uart_putc_raw(uart0, data.axis);
+            uart_putc_raw(uart0, MSB);
+            uart_putc_raw(uart0, LSB);
+            uart_putc_raw(uart0, -1);
+        }
+
     }
 }
 
-int main() {
+void x_task(void *p){
+    adc_t data;
+    adc_init();
+    adc_gpio_init(ADC_PINX);
+    while(true){
+        adc_select_input(ADC_CHANNELX);
+        int result = adc_read();
+        result = result -2048;
+        result = result/8;
+        if(abs(result) < deadzone){
+            result = 0;
+        }
+        else{
+            data.val = result;
+            data.axis = 1;
+            xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+            vTaskDelay(pdMS_TO_TICKS(100));
+        }
+    }
+}
+
+void y_task(void *p){
+    adc_t data;
+    adc_init();
+    adc_gpio_init(ADC_PINY);
+    while(true){
+        adc_select_input(ADC_CHANNELY);
+        int result = adc_read();
+        result = result -2048;
+        result = result/8;
+        if(abs(result) < deadzone){
+            result = 0;
+        }
+        else{
+            data.val = result;
+            data.axis = 0;
+            xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+            vTaskDelay(pdMS_TO_TICKS(100));
+        }
+    }   
+}
+    int main() {
     stdio_init_all();
 
     xQueueAdc = xQueueCreate(32, sizeof(adc_t));
 
     xTaskCreate(uart_task, "uart_task", 4096, NULL, 1, NULL);
+    xTaskCreate(x_task, "x_task", 4096, NULL, 1, NULL);
+    xTaskCreate(y_task, "y_task", 4096, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
